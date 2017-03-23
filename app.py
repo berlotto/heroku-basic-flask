067df37a0e12d23d2d939ad2231aa481c2b30d69
Sergio Berlotto <sergio.berlotto@gmail.com>
2017-03-23 09:41:11 -0300

    Remocao de dados

--- app.py
+++ app.py
@@ -29,11 +29,20 @@ def investir():
 
 @app.route("/investido")
 def investidos():
-    total_zumbi = functools.reduce(lambda x, y: x+y, [int(x['zumbi']) for x in tabela.all()])
-    total_olimpiadas = functools.reduce(lambda x, y: x+y, [int(x['olimpiadas']) for x in tabela.all()])
-    return render_template("resultados.html", total_zumbi=total_zumbi,
-        total_olimpiadas=total_olimpiadas, votos=tabela.all())
+    if tabela.all():
+        total_zumbi = functools.reduce(lambda x, y: x+y, [int(x['zumbi']) for x in tabela.all()])
+        total_olimpiadas = functools.reduce(lambda x, y: x+y, [int(x['olimpiadas']) for x in tabela.all()])
+        return render_template("resultados.html", total_zumbi=total_zumbi,
+            total_olimpiadas=total_olimpiadas, votos=tabela.all())
+    else:
+        return render_template("resultados.html")
 
+
+@app.route("/limpar_tudo")
+def limpar_db():
+    tabela.purge()
+    db.purge()
+    return "Banco limpo!"
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
